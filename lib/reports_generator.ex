2bb7d5e5276e40b7b7ded4ecc86b9d70dda0db6d
Sup3r-Us3r <maydersonmello@gmail.com>
2022-02-13 20:54:49 -0300

    add first version of reports generator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,4 +1,48 @@
 defmodule ReportsGenerator do
-  def call do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options [
+    "users",
+    "foods"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(
+      report_acc(),
+      fn line, report -> sum_values(line, report) end
+    )
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc() do
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+
+    %{"users" => users, "foods" => foods}
   end
 end
