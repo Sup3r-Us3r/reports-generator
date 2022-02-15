5d60feaf8855d424326a9d5b9010a77b05a648d4
Sup3r-Us3r <maydersonmello@gmail.com>
2022-02-14 22:17:43 -0300

    create parallel version

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -26,6 +26,18 @@ defmodule ReportsGenerator do
     )
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report ->
+      sum_reports(report, result)
+    end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
@@ -37,6 +49,20 @@ defmodule ReportsGenerator do
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
     %{report | "users" => users, "foods" => foods}
+  end
+
+  defp sum_reports(
+         %{"users" => users1, "foods" => foods1},
+         %{"users" => users2, "foods" => foods2}
+       ) do
+    users = merge_maps(users1, users2)
+    foods = merge_maps(foods1, foods2)
+
+    %{"users" => users, "foods" => foods}
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc() do
